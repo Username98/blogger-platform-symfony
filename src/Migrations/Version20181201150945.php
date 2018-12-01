0cea7c358332cae9da27825057d9e16773a2a660
Username98 <vlad_hahel@mail.ru>
2018-12-01 18:37:51 +0300

    pagination for all pages

---
+++ src/Migrations/Version20181201150945.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181201150945 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE post CHANGE create_date create_date DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE user ADD short_description VARCHAR(20) NOT NULL, ADD requested_role VARCHAR(20) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE post CHANGE create_date create_date DATE NOT NULL');
+        $this->addSql('ALTER TABLE user DROP short_description, DROP requested_role');
+    }
+}
